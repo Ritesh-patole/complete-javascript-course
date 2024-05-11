4d84fe87953b865be9118d99dd599aa5d6d0c17a
Ritesh <113718896+Ritesh-patole@users.noreply.github.com>
2024-05-11 08:57:57 +0000

    basics

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let js = 'amazing';
+      if(js==='amazing') alert('JavaScript is FUN!');
+
+      sum = 40+8+23-10;
+      console.log(sum);
+
+let firstName = "Ritesh";
+console.log("My name is "+ firstName);
+console.log("My name is "+ firstName);
+console.log("My name is "+ firstName);
+
